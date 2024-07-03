2ee1443dfe63971c8a9f04dcbe8ec1c8d5627978
biggani710 <nguyenvanbebu2k5@gmail.com>
2024-07-03 18:26:34 +0700

    Revert "Giang"
    This reverts commit 146ac0dbf86bd8a422379f80d6e35bf35da9d764.

--- Assets/GIANG/scripts/PlayerBiggani.cs
+++ Assets/GIANG/scripts/PlayerBiggani.cs
@@ -1,45 +1,40 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.Animations;
 
-public class PlayerBiGgani : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    public Vector3 moveInput;
 
-    public Rigidbody2D rb;
+    private Animator animator;
     public SpriteRenderer characterSR;
-    Animator animator;
 
     public float dashBoost = 2f;
     private float dashTime;
     public float DashTime;
     private bool once;
-
-    public Vector3 moveInput;
-
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponentInChildren<Animator>();
+        animator = GetComponent<Animator>();
     }
 
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        /// Part 2
-        // Movement
+        //di chuyển
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         transform.position += moveSpeed * Time.deltaTime * moveInput;
-        //
 
+        //animation
         animator.SetFloat("Speed", moveInput.sqrMagnitude);
 
-        if (Input.GetKeyDown(KeyCode.Space) && dashTime <= 0)
+        //dash
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashTime <= 0)
         {
             animator.SetBool("Dash", true);
             moveSpeed += dashBoost;
@@ -58,11 +53,16 @@ public class PlayerBiGgani : MonoBehaviour
             dashTime -= Time.deltaTime;
         }
 
-        // Rotate Face
         if (moveInput.x != 0)
+        {
             if (moveInput.x < 0)
+            {
                 characterSR.transform.localScale = new Vector3(-1, 1, 0);
+            }
             else
+            {
                 characterSR.transform.localScale = new Vector3(1, 1, 0);
+            }
+        }
     }
 }
