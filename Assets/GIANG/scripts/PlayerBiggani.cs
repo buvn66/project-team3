39dbae1b04d0a1b72e7b035d68edd9ef04ca1ed8
biggani710 <nguyenvanbebu2k5@gmail.com>
2024-07-03 14:04:11 +0700

    Giang

---
+++ Assets/GIANG/scripts/PlayerBiggani.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.Animations;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    public Vector3 moveInput;
+
+    private Animator animator;
+
+    public float dashBoost = 2f;
+    private float dashTime;
+    public float DashTime;
+    private bool once;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    private void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        transform.position += moveSpeed * Time.deltaTime * moveInput;
+
+
+
+        if (moveInput.x != 0)
+        {
+            if (moveInput.x < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 0);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1, 1, 0);
+            }
+        }
+    }
+}
