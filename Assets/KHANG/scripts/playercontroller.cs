a1a93ba1c1a7a9367a9c20063a0857169409ef27
buvn66 <khangnvvps39406@gmail.com>
2024-07-05 21:43:40 +0700

    Playtest

---
+++ Assets/KHANG/scripts/playercontroller.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    
+}
