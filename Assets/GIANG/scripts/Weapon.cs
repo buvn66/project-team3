f13587d0ec793d1aa51335840499e723e7039b74
AnhVu005 <kieuvu100@gmail.com>
2024-07-08 17:07:11 +0700

    Squashed commit of the following:
    commit acd6dde89b8a4a61864b341f6cc5499284c66d73
    Merge: c77b8db b99895e
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Thu Jul 4 14:52:21 2024 +0700
    
        Merge pull request #2 from buvn66/Giang
    
        Map
    
    commit b99895e1fc8e06afdf29ca279093885a19b4b372
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Thu Jul 4 14:49:32 2024 +0700
    
        Map
    
    commit c77b8dbef77743c9e50e01f1eeaf3c02228fdda0
    Merge: 2b8704f 28bcb37
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 21:59:14 2024 +0700
    
        Merge pull request #1 from buvn66/Giang
    
        Nhặt đồ
    
    commit 28bcb37377cb945a25e390e9360bd2ef5ddc3cb7
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 21:54:34 2024 +0700
    
        Giang
    
    commit 440fad2c01aff37e10c33b9e6ba74a9073e886c3
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 21:15:15 2024 +0700
    
        Giang
    
    commit e8cadd8a01cc1a71da3d481a3f5949502ba2264d
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 21:06:15 2024 +0700
    
        Giang
    
    commit d821de0351934e0460139e36fe21d33d4c899300
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 20:32:43 2024 +0700
    
        Giang
    
    commit 49bbc3486968ac0b7f90964b579640c87e3af0f9
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 19:45:12 2024 +0700
    
        Giang
    
    commit a943c937533e0cfe0985eaa49fbb8118ae4582a9
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 18:36:32 2024 +0700
    
        Giang
    
    commit 2ee1443dfe63971c8a9f04dcbe8ec1c8d5627978
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 18:26:34 2024 +0700
    
        Revert "Giang"
    
        This reverts commit 146ac0dbf86bd8a422379f80d6e35bf35da9d764.
    
    commit 146ac0dbf86bd8a422379f80d6e35bf35da9d764
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 18:21:44 2024 +0700
    
        Giang
    
    commit 5ce48281407e3f48b36bf94513a58ea92a7866a3
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 17:27:47 2024 +0700
    
        Giang
    
    commit 20bd7fa9962514d7ab078952fb1ad25b4fc4f7b6
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 17:08:13 2024 +0700
    
        Giang
    
    commit 94159e74a23ba74d4b832fd186c29ee14cabd779
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 15:48:52 2024 +0700
    
        Giang
    
    commit 0c5c6e5ad6bf025373fe182fac10ca7d2c0339fe
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 15:28:43 2024 +0700
    
        Giang
    
    commit 29c4cb1fe6c207633c5f18db46e593712846aa28
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 14:17:13 2024 +0700
    
        Giang
    
    commit d2125aa9f69c541b7e85ecac0d9df96335ea83da
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 14:13:38 2024 +0700
    
        Giang
    
    commit 39dbae1b04d0a1b72e7b035d68edd9ef04ca1ed8
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 14:04:11 2024 +0700
    
        Giang
    
    commit ac3690fd84e89f5ebc8b38bcf1f3678a0e5dea93
    Author: biggani710 <nguyenvanbebu2k5@gmail.com>
    Date:   Wed Jul 3 12:25:49 2024 +0700
    
        Giang

---
+++ Assets/GIANG/scripts/Weapon.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private bool playerInRange = false;
+    private bool canPickUp = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            canPickUp = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            canPickUp = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (playerInRange && canPickUp)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2, 100, 20), "Press F to pick up");
+        }
+    }
+
+    void Update()
+    {
+        if (playerInRange && canPickUp && Input.GetKeyDown(KeyCode.F))
+        {
+            PickUpWeapon();
+        }
+    }
+
+    void PickUpWeapon()
+    {
+        // Xóa vũ khí hiện tại từ hierarchy
+        Destroy(gameObject);
+
+        // Hiển thị vũ khí có sẵn trên hierarchy (ví dụ: sử dụng PlayerController để trigger hiển thị vũ khí)
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBiggani>().ShowCurrentWeapon();
+    }
+}
