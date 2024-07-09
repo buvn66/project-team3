ec8fffbd215ad3cd228275413a91363e332ffa74
buvn66 <khangnvvps39406@gmail.com>
2024-07-10 02:37:01 +0700

    bullet

--- Assets/KHANG/scripts 1/topdown/playercontroller.cs
+++ Assets/KHANG/scripts 1/topdown/playercontroller.cs
@@ -13,21 +13,31 @@ public class Playercontroller : MonoBehaviour
     private Vector2 moveInput;
     private Animator animator;
     private Camera mainCamera;
+    public GameObject bulletPrefabs;
+    public Transform firePoint;
+    public float fireRate = 1.0f;
+    private float nextfireTime;
+
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        
         mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-       
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));        
         RotatePlayer();
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextfireTime)
+        {
+            shoot();
+            nextfireTime = Time.time + fireRate;
+
+        }
     }
 
     private void FixedUpdate()
@@ -36,7 +46,7 @@ public class Playercontroller : MonoBehaviour
 
 
     }
-    
+   
     void RotatePlayer()
     {
         Vector2 mousePositison = mainCamera.ScreenToWorldPoint(Input.mousePosition);
@@ -44,5 +54,9 @@ public class Playercontroller : MonoBehaviour
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
     }
+    void shoot()
+    {
+        Instantiate(bulletPrefabs, firePoint.position, firePoint.rotation);
 
+    }
 }
