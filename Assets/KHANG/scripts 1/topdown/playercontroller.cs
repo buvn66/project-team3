831df67f7bc604025f70f1191aac32244a9f6a20
buvn66 <khangnvvps39406@gmail.com>
2024-07-09 20:08:39 +0700

    mousePositison topdown

--- Assets/KHANG/scripts 1/topdown/playercontroller.cs
+++ Assets/KHANG/scripts 1/topdown/playercontroller.cs
@@ -7,21 +7,38 @@ public class Playercontroller : MonoBehaviour
     public float moveSpeed = 2f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
+    private Camera mainCamera;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+       
+        RotatePlayer();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = moveInput.normalized * moveSpeed;
+
+
     }
+    
+    void RotatePlayer()
+    {
+        Vector2 mousePositison = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDirection = mousePositison - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
+
 }
