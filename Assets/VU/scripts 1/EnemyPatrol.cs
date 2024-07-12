abb8f78cb6dd4375ebe7bafd69d8d1d3e93ef89b
AnhVu005 <kieuvu100@gmail.com>
2024-07-12 23:54:24 +0700

    vu update 12/7

---
+++ Assets/VU/scripts 1/EnemyPatrol.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float moveSpeed = 2f; // Tốc độ di chuyển của quái
+    public float start, end; // điểm bắt đầu , điểm kết thúc 
+    private Vector3 targetPosition; // Vị trí mục tiêu để di chuyển đến
+
+    void Start()
+    {
+        // Ban đầu quái vật sẽ di chuyển tới điểm bắt đầu
+        targetPosition = new Vector3(start, transform.position.y, transform.position.z);
+    }
+
+    void Update()
+    {
+        // Di chuyển con quái vật đến vị trí mục tiêu
+        MoveToTarget();
+    }
+
+    // Hàm di chuyển con quái vật đến vị trí mục tiêu
+    void MoveToTarget()
+    {
+        // Tính toán hướng di chuyển của quái vật
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+        // Xoay mặt quái vật theo hướng di chuyển
+        if (moveDirection.x > 0)
+        {
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        }
+        else if (moveDirection.x < 0)
+        {
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+
+        // Kiểm tra nếu quái vật đã gần đến vị trí mục tiêu, thay đổi mục tiêu mới
+        if (Mathf.Abs(transform.position.x - targetPosition.x) < 0.1f)
+        {
+            // Đổi hướng di chuyển tới điểm kết thúc nếu đã đến điểm bắt đầu
+            if (targetPosition.x == start)
+            {
+                targetPosition.x = end;
+            }
+            // Đổi hướng di chuyển tới điểm bắt đầu nếu đã đến điểm kết thúc
+            else if (targetPosition.x == end)
+            {
+                targetPosition.x = start;
+            }
+        }
+    }
+}
--- Assets/VU/scripts 1/Enemyheath.cs
+++ Assets/VU/scripts 1/Enemyheath.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,7 @@ public class Enemyheath : MonoBehaviour
     public float heath;
     public float currentHeath;
     private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +27,13 @@ public class Enemyheath : MonoBehaviour
         {
             anim.SetBool("dead", true);
             Debug.Log("Enemy is dead");
+            StartCoroutine(DisappearAfterDeath());
         }
+    }
+
+    private IEnumerator DisappearAfterDeath()
+    {
+        yield return new WaitForSeconds(3); // Wait for 3 seconds
+        Destroy(gameObject); // Destroy the enemy game object
     }
 }
