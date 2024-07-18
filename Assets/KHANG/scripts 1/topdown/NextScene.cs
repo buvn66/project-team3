e532500ce4316126ba1cd85050858f62bb2c5d6c
buvn66 <khangnvvps39406@gmail.com>
2024-07-18 14:46:59 +0700

    xong cutscene 1

--- Assets/KHANG/scripts 1/topdown/NextScene.cs
+++ Assets/KHANG/scripts 1/topdown/NextScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class NextScene : MonoBehaviour
@@ -7,12 +8,18 @@ public class NextScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(LoadNextScene());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(53.3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
