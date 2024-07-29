8738284738be2cd85d1debb98da0f223e4527e6b
buvn66 <khangnvvps39406@gmail.com>
2024-07-30 03:49:13 +0700

    cập nhật boss2

---
+++ Assets/KHANG/scripts 1/boss/Knight.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Knight : MonoBehaviour
+{
+    [SerializeField]
+    private float leftBoundary;
+
+
+    [SerializeField]
+    private float rightBoundary;
+
+
+    [SerializeField]
+    private float moveSpeed = 20f;
+
+
+
+    [SerializeField]
+    private bool _isMovingRight = true;
+
+    //máu của Boss 
+    private float _health = 100;
+
+    [SerializeField]
+    private Slider _healthSlider;
+
+    public UnityEvent bossKilledEvent;
+
+
+    void Update()
+    {       
+        var currentPosition = transform.localPosition;
+        if (currentPosition.x > rightBoundary)
+        {
+
+            _isMovingRight = false;
+
+        }
+        else if (currentPosition.x < leftBoundary)
+        {
+ 
+            _isMovingRight = true;
+        }       
+        var direction = Vector3.right;
+        if (_isMovingRight == false)
+        {
+            direction = Vector3.left;
+        }
+        transform.Translate(direction * moveSpeed * Time.deltaTime);       
+    }
+    private void Start()
+    {
+        _healthSlider.value = _health;
+    }   
+}
---
+++ Assets/KHANG/scripts 1/topdown/gun.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;  // Prefab của đạn
+    public Transform firePoint;  // Vị trí bắn đạn
+    public float bulletSpeed = 10f;  // Tốc độ của đạn
+    public float fireRate = 2f;  // Tốc độ bắn (giây giữa mỗi lần bắn)
+
+    private float fireTimer;  // Bộ đếm thời gian cho tốc độ bắn
+
+    void Start()
+    {
+        fireTimer = fireRate;  // Khởi tạo bộ đếm thời gian
+    }
+
+    void Update()
+    {
+        fireTimer -= Time.deltaTime;  // Giảm bộ đếm thời gian theo thời gian đã trôi qua
+
+        if (fireTimer <= 0f)
+        {
+            Shoot();  // Bắn đạn
+            fireTimer = fireRate;  // Đặt lại bộ đếm thời gian
+        }
+    }
+
+    void Shoot()
+    {
+        // Tạo một viên đạn từ prefab tại vị trí bắn
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        // Thêm lực đẩy cho viên đạn theo hướng firePoint với tốc độ bulletSpeed
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = firePoint.right * bulletSpeed;
+    }
+}
