f9af9d83f5bd28cf076625d225912858596944f3
biggani710 <nguyenvanbebu2k5@gmail.com>
2024-07-29 17:04:05 +0700

    Fix 20 lỗi

--- Assets/KIET/Assets/Scripts/Boss_Run.cs
+++ Assets/KIET/Assets/Scripts/Boss_Run.cs
@@ -1,39 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Boss_Run : StateMachineBehaviour
-{
-    public float speed = 2.5f;
-    public float attackRange = 3f;
+//public class Boss_Run : StateMachineBehaviour
+//{
+//    public float speed = 2.5f;
+//    public float attackRange = 3f;
 
-    Transform player;
-    Rigidbody2D rb;
-    Boss boss;
+//    Transform player;
+//    Rigidbody2D rb;
+//    Boss boss;
 
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = animator.GetComponent<Rigidbody2D>();
-        boss = animator.GetComponent<Boss>();
-    }
+//    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+//    {
+//        player = GameObject.FindGameObjectWithTag("Player").transform;
+//        rb = animator.GetComponent<Rigidbody2D>();
+//        boss = animator.GetComponent<Boss>();
+//    }
 
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        boss.LookAtPlayer();
+//    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+//    {
+//        boss.LookAtPlayer();
 
-        Vector2 target = new Vector2(player.position.x, rb.position.y);
-        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
-        rb.MovePosition(newPos);
+//        Vector2 target = new Vector2(player.position.x, rb.position.y);
+//        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+//        rb.MovePosition(newPos);
 
-        if (Vector2.Distance(player.position, rb.position) <= attackRange)
-        {
-            animator.SetTrigger("Attack");
-        }
-    }
+//        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+//        {
+//            animator.SetTrigger("Attack");
+//        }
+//    }
 
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.ResetTrigger("Attack");
-    }
-}
+//    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+//    {
+//        animator.ResetTrigger("Attack");
+//    }
+//}
