3979c3cf6682ebb0d4c7a135034c9a10688b7e75
biggani710 <nguyenvanbebu2k5@gmail.com>
2024-07-30 01:31:52 +0700

    Fix máu với thêm destroy cho cube

--- Assets/GIANG/scripts/GameController.cs
+++ Assets/GIANG/scripts/GameController.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] int score = 0;
     [SerializeField] TextMeshProUGUI ScoreText;
+    public Image FillBar;
+    public TextMeshProUGUI ValueText;
     void Start()
     {
         ScoreText.text = score.ToString();
@@ -19,5 +23,10 @@ public class GameController : MonoBehaviour
     {
         score += scoreToAdd;
         ScoreText.text = score.ToString();
+    }
+    public void UpdateBar(int currentValue, int MaxValue)
+    {
+        FillBar.fillAmount = (float)currentValue / (float)MaxValue;
+        ValueText.text = currentValue.ToString() + "/" + MaxValue.ToString();
     }
 }
