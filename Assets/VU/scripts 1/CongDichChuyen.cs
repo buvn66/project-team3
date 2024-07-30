91d57bc5f87b5835ce39cc66820228c6c861d9dd
AnhVu005 <kieuvu100@gmail.com>
2024-07-30 21:30:59 +0700

    30/7

---
+++ Assets/VU/scripts 1/CongDichChuyen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CongDichChuyen : MonoBehaviour
+{
+    [SerializeField] Transform diemDichChuyenDen;
+    public Transform GetDiemDichChuyen()
+    {
+        return diemDichChuyenDen;
+    }
+}
+
